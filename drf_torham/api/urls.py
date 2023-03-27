de3c89d2024ace9f6e8eedc4b8a2a16dc593a7fc
Bahman Pournazari <bahmanpn@gmail.com>
2023-03-27 22:41:58 +0430

    4-update and delete mehtods for function and basic class base view with APIView

--- drf_torham/api/urls.py
+++ drf_torham/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', user_profile, name='user-profile-drf'),
     path('comments/', comments, name='comments-drf'),
+    path('', user_profile, name='user-profile-drf'),
+    path('<int:pk>/', update_delete_user_profile, name='update_delete_user-profile-drf'),
+    path('users/', UserProfileView.as_view(), name='user-profile-class-base-view-get-post'),
+    path('users/<pk>/', UserProfileView.as_view(), name='user-profile-class-base-view-update-and-delete'),
 ]
--- drf_torham/api/views.py
+++ drf_torham/api/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import UserProfile, Comment
 from .serializers import UserProfileSerializer, CommentSerializer, CreateUserProfileSerializer
+from rest_framework import status
 
+
+# @api_view(['GET', 'POST'])
+# def user_profile(request):
+#     return Response({'data': 'bahman'})
 
 @api_view(['GET', 'POST'])
 def user_profile(request):
@@ -17,15 +24,71 @@ def user_profile(request):
 
         serializer.is_valid(raise_exception=True)  # raise_exception means that if errors exists return them!!
         serializer.save()
-        return Response(serializer.data, status=201)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# @api_view(['GET', 'POST'])
-# def user_profile(request):
-#     return Response({'data': 'bahman'})
+@api_view(['GET', 'PUT', 'DELETE'])
+def update_delete_user_profile(request, pk):
+    if request.method == 'GET':
+        profile = UserProfile.objects.get(pk=pk)
+        if profile is not None:
+            serializer_data = UserProfileSerializer(profile)
+            return Response(serializer_data.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        user_profile_pk = UserProfile.objects.get(pk=pk)
+        if user_profile_pk is not None:
+            serializer = UserProfileSerializer(
+                instance=user_profile_pk,
+                data=request.data
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        user_profile_pk = UserProfile.objects.get(pk=pk)
+        user_profile_pk.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['GET', 'POST'])
 def comments(request):
     comments = Comment.objects.all()
     serializer_data = CommentSerializer(comments, many=True)
     return Response(serializer_data.data)
+
+
+class UserProfileView(APIView):
+    def get(self, request):
+        profiles = UserProfile.objects.all()
+
+        serializer_data = UserProfileSerializer(profiles, many=True)
+        return Response(serializer_data.data)
+
+    def post(self, request):
+        serializer = CreateUserProfileSerializer(data=request.data)
+        # we don't want to fill instance and return ir like get method,we want to fill data with this serializer
+        serializer.is_valid(raise_exception=True)
+        # raise_exception means that if errors exists return them!!
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, pk):
+        user_profile_pk = UserProfile.objects.get(pk=pk)
+        if user_profile_pk is not None:
+            serializer = UserProfileSerializer(
+                instance=user_profile_pk,
+                data=request.data
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        user_profile_pk = UserProfile.objects.get(pk=pk)
+        user_profile_pk.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
